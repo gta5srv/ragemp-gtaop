bbaa5aea8c69475c656ae6efee72876f1b0fbfa1
Alexander Roidl <alexanderroidl@gmail.com>
2020-03-12 01:35:45 +0100

    Added server time class and time randomization/increment
    Former-commit-id: 9991143112a7a846a28b822592dd4f713d02fbe6

--- projects/server/src/lib/algebra/random.ts
+++ projects/server/src/lib/algebra/random.ts
@@ -1,8 +1,18 @@
 export default class Random {
+  /**
+   * Getting a random integer between two values
+   */
   static getInt(min: number, max: number): number {
     min = Math.ceil(min)
     max = Math.floor(max)
 
     return Math.floor(Math.random() * (max - min)) + min
+  }
+
+  static getIntInclusive(min: number, max: number): number {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+
+    return Math.floor(Math.random() * (max - min + 1)) + min
   }
 }
--- projects/server/src/lib/core/server.ts
+++ projects/server/src/lib/core/server.ts
@@ -1,20 +1,23 @@
 import HeightMap from '@lib/height-map'
 import Interval from '@lib/algebra/interval'
 import Rectangle from '@lib/algebra/rectangle'
+import Random from '@lib/algebra/random'
 import TeamManager from '@lib/managers/team-manager'
 import ClientManager from '@lib/managers/client-manager'
 import ZoneManager from '@lib/managers/zone-manager'
 import VehicleManager from '@lib/managers/vehicle-manager'
 
-export default class Server {
-  static debuggingEnabled: boolean = true
-  static heightMap: HeightMap
 
-  static readonly clients: ClientManager = new ClientManager()
-  static readonly teams: TeamManager = new TeamManager()
-  static readonly zones: ZoneManager = new ZoneManager()
-  static readonly vehicles: VehicleManager = new VehicleManager()
+/**
+ * The Gamemode's heart
+ */
+class Server {
+  public static heightMap: HeightMap
 
+  public static readonly clients: ClientManager = new ClientManager()
+  public static readonly teams: TeamManager = new TeamManager()
+  public static readonly zones: ZoneManager = new ZoneManager()
+  public static readonly vehicles: VehicleManager = new VehicleManager()
 
   private static loopTimer: NodeJS.Timeout|null = null
   private static loopLastRun: Date|null = null
@@ -22,8 +25,14 @@ export default class Server {
 
   private static readonly GAME_TIME_MULTIPLIER: number = 50
   private static readonly TICK_RATE: number = 200
+  private static readonly DEBUG_MODE: boolean = true
 
 
+  /**
+   * Initialize height map
+   *
+   * @param heightMapPath The file path to the hmap.dat file
+   */
   public static initHeightMap (heightMapPath: string): void {
     const v = new mp.Vector3(-4100, -4300, 0)
     const v2 = v.add(new mp.Vector3(300 * 30, 150 * 90, 0))
@@ -32,28 +41,57 @@ export default class Server {
     Server.heightMap = new HeightMap(heightMapPath, area)
   }
 
-  static log (...args: any[]): void {
+
+  /**
+   * Logs to console
+   *
+   * @param ...args Message parts
+   */
+  public static log (...args: any[]): void {
     console.log(...args)
   }
 
-  static debug (...args: any[]): void {
-    if (!Server.debuggingEnabled) {
-      return
-    }
 
-    Server.log(...args)
+  /**
+   * Prints debugging to console if DEBUG_MODE is true
+   *
+   * @param ...args Message parts
+   */
+  public static debug (...args: any[]): void {
+    if (Server.DEBUG_MODE) {
+      Server.log(...args)
+    }
   }
 
-  static sendMessageToAll (...args: any[]): void {
+
+  /**
+   * Sends a message to all players online
+   *
+   * @param ...args Message parts
+   */
+  public static sendMessageToAll (...args: any[]): void {
     Server.clients.sendMessage(...args)
   }
 
-  static broadcast (...args: any[]): void {
+
+  /**
+   * Broadcasts a message to console and players
+   *
+   * @param ...args Message parts
+   */
+  public static broadcast (...args: any[]): void {
     Server.log(...args)
     Server.sendMessageToAll(...args)
   }
 
-  static playerArgsToClientArray (...args: any[]): any[] {
+
+  /**
+   * Converts the first argument of an array from PlayerMp to Client instance
+   *
+   * @param  ...args Argument list
+   * @return         Argument list with Client instance first
+   */
+  private static playerArgsToClientArray (...args: any[]): any[] {
     let player = args.shift()
 
     let client = Server.clients.byPlayerMp(player)
@@ -62,13 +100,23 @@ export default class Server {
     return args
   }
 
-  static addCommand (command: string, callback: Function): void {
+
+  /**
+   * Adds a command (wrapper for mp.events.addCommand) and calls callback
+   * with first argument as Client instance instead of PlayerMp
+   */
+  public static addCommand (command: string, callback: Function): void {
     mp.events.addCommand(command, function (...args: any[]) {
       callback.apply(null, Server.playerArgsToClientArray(...args))
     })
   }
 
-  static addEvent (event: string, callback: Function): void {
+
+  /**
+   * Adds an event (wrapper for mp.events.add) and calls callback
+   * with first argument as Client instance instead of PlayerMp
+   */
+  public static addEvent (event: string, callback: Function): void {
     mp.events.add(event, function (...args: any[]) {
       callback.apply(null, Server.playerArgsToClientArray(...args))
     })
@@ -79,6 +127,7 @@ export default class Server {
    * Starts cricital server functionality
    */
   public static start (): void {
+    Server.Time.randomize()
     Server.runLoop()
   }
 
@@ -132,5 +181,94 @@ export default class Server {
    * @param msElapsed Milliseconds elapsed since last run
    */
   private static loop (msElapsed: number): void {
+    Server.msSinceTimeIncrease += msElapsed
+
+    // At least a second has passed since last in-game time increase
+    const gameMsSinceTimeIncreased = Server.msSinceTimeIncrease * Server.GAME_TIME_MULTIPLIER
+
+    if (gameMsSinceTimeIncreased > 1000) {
+      const gameSecsToIncrease = Math.floor(gameMsSinceTimeIncreased / 1000)
+
+      Server.Time.add(gameSecsToIncrease)
+      Server.msSinceTimeIncrease -= gameSecsToIncrease * 1000 / Server.GAME_TIME_MULTIPLIER
+    }
   }
 }
+
+namespace Server {
+  export class Time {
+    static get hour () {
+      return mp.world.time.hour
+    }
+
+    static set hour (hour: number) {
+      mp.world.time.hour = hour
+    }
+
+    static get minute () {
+      return mp.world.time.minute
+    }
+
+    static set minute (minute: number) {
+      mp.world.time.minute = minute
+    }
+
+    static get second () {
+      return mp.world.time.second
+    }
+
+    static set second (second: number) {
+      mp.world.time.second = second
+    }
+
+    public static set (hour: number, minute: number, second: number): void {
+      mp.world.time.set(hour, minute, second)
+    }
+
+    public static randomize (): void {
+      Server.Time.set(
+        Random.getIntInclusive(0, 23),
+        Random.getIntInclusive(0, 59),
+        Random.getIntInclusive(0, 59)
+      )
+    }
+
+    public static add (secondsToAdd: number): void {
+      secondsToAdd = Math.floor(secondsToAdd)
+
+      let hour = Server.Time.hour
+      let minute = Server.Time.minute
+      let second = Server.Time.second + secondsToAdd
+
+      if (second > 59) {
+        minute += Math.floor(second / 60)
+        second = second % 60
+
+        if (minute > 59) {
+          hour += Math.floor(minute / 60)
+          minute = minute % 60
+
+          if (hour > 23) {
+            hour = hour % 24
+          }
+        }
+      }
+
+      Server.Time.hour = hour
+      Server.Time.minute = minute
+      Server.Time.second = second
+    }
+
+    public static toString () {
+      return [
+        Server.Time.hour,
+        Server.Time.minute,
+        Server.Time.second
+      ].map((timeValue: number): string => {
+        return ('0' + timeValue).slice(-2)
+      }).join(':')
+    }
+  }
+}
+
+export default Server
